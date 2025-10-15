2f795ec029196f955ab0ba31b4f4e1f1ac89a22a
kul0625 <kuldeepk6996@gmail.com>
2025-10-16 01:29:42 +0530

    Delete Question1 directory

--- Question1/src/Routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {getAllUsers } = require('../Controllers/authController');
-const auth = require('../Middleware/Auth');
-const router = express.Router();
-
-router.get('/', getAllUsers);
-
-module.exports = router;
